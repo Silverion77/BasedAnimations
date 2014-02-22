3a423ea280a29c354146181e20dd4490e0b1b1b7
Koakuma <icethrush@gmail.com>
2014-02-22 18:18:20 -0500

    Included code and stuff

---
+++ Assignment1/src/cs5643/particles/Constants.java
@@ -0,0 +1,18 @@
+package cs5643.particles;
+
+/**
+ * Default constants. Add your own as necessary.
+ * 
+ * @author Doug James, January 2007
+ * @author Eston Schweickart, February 2014
+ */
+public interface Constants
+{
+    /** Mass of a particle. */
+    public static final double PARTICLE_MASS     = 1.0;
+
+    /** Camera rotation speed constants. */
+    public static final double CAM_SIN_THETA     = Math.sin(0.2);
+    public static final double CAM_COS_THETA     = Math.cos(0.2);
+
+}
